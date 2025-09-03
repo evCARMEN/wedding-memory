4e100904baddae8b8a0e1752bb9b8958fa16162a
evCARMEN <td@carmen-ev.de>
2025-09-03 15:50:56 +0200

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,13 +1,15 @@
 // Trage hier deine Werte aus der Firebase‑Konsole ein
 // Beispiel:
-const firebaseConfig = {
+// public/firebase-config.js
+window.firebaseConfig = {
   apiKey: "AIzaSyA_C8_WCaw7I7E9FRCQLHIhjd2RRppaFo4",
   authDomain: "hochzeits-memory-prod.firebaseapp.com",
   projectId: "hochzeits-memory-prod",
-  storageBucket: "hochzeits-memory-prod.firebasestorage.app",
+  storageBucket: "hochzeits-memory-prod.appspot.com",  // ✅ korrigiert
   messagingSenderId: "900383167774",
-  appId: "1:900383167774:web:7b40c77dc3b24b69f4650f",
+  appId: "1:900383167774:web:7b40c77dc3b24b69f4650f"
 };
+
 
 
 // Fail‑safe: Blockiere App, wenn Config fehlt
